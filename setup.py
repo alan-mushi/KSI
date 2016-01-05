72ecc4790ca74ed5c26eeabf462b84bf7e45c265
Thibault Guittet <guittet.thibault[at]gmail.com>
2016-01-05 10:07:45 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='ksi_git',
+        version='0',
+        packages=[''],
+        url='https://github.com/alan-mushi/KSI/',
+        license='',
+        author='',
+        author_email='',
+        description=''
+)
