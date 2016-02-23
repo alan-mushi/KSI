dc51cd9c335b1e1b3eea4975468d81c9d98d7c3b
Thibault Guittet <guittet.thibault[at]gmail.com>
2016-02-23 16:17:09 +0100

    Chart of all timings (Fix #8)

--- tests/benchmarks/graph_formatter.py
+++ tests/benchmarks/graph_formatter.py
@@ -1,25 +1,37 @@
 import os
 import json
+from datetime import datetime
 
-DIR = '.'
-START_EXPR = 'benchmark-Keys-{}-l_'.format('md5')
+DIR = '/home/user/Desktop/ksi_shelves/'
+START_EXPR = 'benchmark-Keys-{}-l_'
 EXTENSION = '.log'
 MOTIF = '__init__'
 SEPARATOR = '|'
+alg_names = ['md5', 'sha256', 'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512', 'whirlpool']
 
 if __name__ == '__main__':
-    files = [f for f in os.listdir(DIR) if os.path.isfile(f) and f.endswith(EXTENSION) and f.startswith(START_EXPR)]
-    val = sorted(list(map(lambda f: f[len(START_EXPR):][:-len(EXTENSION)], files)), key=int)
+    os.chdir(DIR)
+    base_date = datetime.utcnow().isoformat(sep='T').split('T')[0]
+    chart_values_all = []
 
-    chart_values = []
-    for i in val:
-        with open(START_EXPR + i + EXTENSION, 'r') as f:
-            for line in f.readlines():
-                if MOTIF in line:
-                    chart_values.append({'time': line.split(sep=SEPARATOR)[1][:-1], 'value': int(i)})
+    for alg in alg_names:
+        START_EXPR = 'benchmark-Keys-{}-l_'.format(alg)
+
+        files = [f for f in os.listdir(DIR) if os.path.isfile(f) and f.endswith(EXTENSION) and f.startswith(START_EXPR)]
+        val = sorted(list(map(lambda f: f[len(START_EXPR):][:-len(EXTENSION)], files)), key=int)
+        chart_values = []
+
+        for i in val:
+            with open(START_EXPR + i + EXTENSION, 'r') as f:
+                for line in f.readlines():
+                    if MOTIF in line:
+                        time = base_date + 'T0' + line.split(sep=SEPARATOR)[1][:-1]
+                        chart_values.append({'time': time, 'value': int(i)})
+
+        chart_values_all.append(chart_values)
 
     metadata = {}
-    metadata['title'] = 'Time spent generating user\'s Keys (Merkle Tree) with MD5'
+    metadata['title'] = 'Time spent generating user\'s Keys (Merkle Tree) with various algorithms'
     metadata['description'] = ''
     metadata['x_accessor'] = 'time'
     metadata['x_label'] = 'Time'
@@ -28,11 +40,17 @@ if __name__ == '__main__':
     metadata['y_label'] = 'l'
     metadata['yax_units'] = ''
     metadata['y_scale_type'] = 'linear'  # 'log' || 'linear'
-    metadata['chart_type'] = 'point'
-    metadata['x_rug'] = False
-    metadata['y_rug'] = False
+    metadata['chart_type'] = 'line'
+    metadata['x_rug'] = True
+    metadata['y_rug'] = True
     metadata['european_clock'] = False
-    metadata['x_rollover_format'] = '%M min %S.%L secs for l = '
+    metadata['legend'] = alg_names
+    metadata['legend_target'] = '#legend'
+    metadata['x_mouseover'] = '%M min %S.%L secs for l = '
+    metadata['colors'] = ['blue', 'orange', 'green', 'yellow', 'brown', 'black', 'fuchsia']
+    metadata['aggregate_rollover'] = True
+    metadata['missing_is_hidden'] = True
+    metadata['full_width'] = True
 
-    with open('../static/data.json', 'w+') as outfile:
-        outfile.write(json.dumps({'data': chart_values, 'metadata': metadata}, indent=4))
+    with open('data.json', 'w+') as outfile:
+        outfile.write(json.dumps({'data': chart_values_all, 'metadata': metadata}, indent=4))
